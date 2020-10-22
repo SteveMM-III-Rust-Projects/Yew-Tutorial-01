f6a8075f37b9c4199dbdd5eb364ccaaa57cd49a9
Kronykus <MoreheadSteve@gmail.com>
2020-10-22 10:38:46 -0500

    console

--- src/app.rs
+++ src/app.rs
@@ -1,67 +1,82 @@
-use yew::prelude::*;
 use rand::prelude::*;
+use yew::prelude::*;
+use yew::services::ConsoleService as console;
 
 
 pub enum Msg {
-  AddOne,
-  RemoveOne,
+   AddOne,
+   RemoveOne,
 }
 
 
 pub struct App {
-  items: Vec<i64>,
-  counter: i64,
-  link: ComponentLink<Self>,
+   items: Vec<i64>,
+   counter: i64,
+   link: ComponentLink<Self>,
 }
 
 
 impl Component for App {
-  type Message = Msg;
-  type Properties = ();
-
-  fn create( _: Self::Properties, link: ComponentLink<Self> ) -> Self {
-    return App {
-      link,
-      items: Vec::new(),
-      counter: 0,
-    };
-  }
+   type Message = Msg;
+   type Properties = ();
 
 
-  fn update(&mut self, msg: Self::Message) -> ShouldRender {
-    match msg {
-      Msg::AddOne => {self.counter += 1; self.items.push( random() );},
-      Msg::RemoveOne => {
-        self.counter -= if self.counter == 0 {0} else {1};
-        self.items.pop();
+   fn create( _: Self::Properties, link: ComponentLink<Self> ) -> Self {
+      return App {
+         link,
+         items: Vec::new(),
+         counter: 0,
+      };
+   }
+
+
+   fn update( &mut self, msg: Self::Message ) -> ShouldRender {
+      match msg {
+         Msg::AddOne => {
+            let added = random();
+            self.counter += 1;
+            self.items.push( added );
+            console::info( format!( "Added {}", added ).as_str() );
+         }
+         Msg::RemoveOne => {
+            self.counter -= if self.counter == 0 { 0 } else { 1 };
+            let removed = self.items.pop();
+            match removed {
+               Some(x) => console::warn( format!( "Removed {}", x ).as_str() ),
+               None    => console::error( "Nothing to remove!" )
+            }
+            
+         }
       }
-    }
 
-    return true;
-  }
-
-
-  fn view( &self ) -> Html {
-    let render_item = |item| {
-      html! {
-        <li> {item} </li>
-      }
-    };
-    return html! {
-      <div class="app center">
-        <p> {"Number of items: "} { self.counter } </p>
-        <p> {"Items: "}</p>
-        <ul>
-          { for self.items.iter().map( render_item ) }
-        </ul>
-        <button onclick=self.link.callback( |_| Msg::AddOne )>{ "+" }</button>
-        <button onclick=self.link.callback( |_| Msg::RemoveOne )>{ "-" }</button>
-      </div>
-    };
-  }
+      return true;
+   }
 
 
-  fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-    return true;
-  }
+   fn view( &self ) -> Html {
+      let render_item = |item| {
+         html! {
+            <li> {item} </li>
+         }
+      };
+
+      return html! {
+         <div class="app center">
+            <p> {"Number of items: "} { self.counter } </p>
+            <p> {"Items: "}</p>
+
+            <ul>
+               { for self.items.iter().map( render_item ) }
+            </ul>
+
+            <button onclick=self.link.callback( |_| Msg::AddOne    )>{ "+" }</button>
+            <button onclick=self.link.callback( |_| Msg::RemoveOne )>{ "-" }</button>
+         </div>
+      };
+   }
+
+
+   fn change( &mut self, _props: Self::Properties ) -> ShouldRender {
+      return true;
+   }
 }
